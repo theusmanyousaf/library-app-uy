c69faf35e18af717700503777a630e05301537d3
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-06-15 17:53:17 +0500

    search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,15 @@
 
 export default function Search() {
-  return (
-    <>
-        <h1 className='flex mt-8 font-bold text-4xl justify-center'>Search Page</h1>
-    </>
-  )
+    return (
+        <>
+            <div className='grid my-20 justify-center'>
+                <div>
+                    <h1 className="font-bold text-4xl mt-12">Search Books</h1>
+                </div>
+                <div className='mt-12'>
+                    <input className="border-gray-100 rounded py-4 pl-4 pr-52" placeholder="Search query" name="searchbar" />
+                </div>
+            </div>
+        </>
+    )
 }
