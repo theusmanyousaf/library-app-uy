e984a7501e7a6474a41acc32081a336d7b0af83e
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-06-26 05:48:53 +0500

    related books fetched with api

--- src/components/relatedBooks/RelatedBooks.tsx
+++ src/components/relatedBooks/RelatedBooks.tsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { Link } from 'react-router-dom';
 import { VscArrowCircleRight, VscArrowCircleLeft } from "react-icons/vsc";
 import '../../styles/index.css'
+import { useParams } from 'react-router-dom';
 
 const RelatedBooks = () => {
+    const { id } = useParams<Record<string, string>>();
+    
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const books = [
-        { id: 1, title: 'Book 1' },
-        { id: 2, title: 'Book 2' },
-        { id: 3, title: 'Book 3' },
-        { id: 4, title: 'Book 4' },
-        { id: 5, title: 'Book 5' },
-        { id: 6, title: 'Book 6' },
-        { id: 7, title: 'Book 7' },
-        { id: 8, title: 'Book 8' },
-        { id: 9, title: 'Book 9' },
-        { id: 10, title: 'Book 10' },
-        { id: 11, title: 'Book 11' },
-        { id: 12, title: 'Book 12' },
-    ];
+    const books = useSelector((state: RootState) => state.books.books).filter(book => book.id !== id);
 
     const visibleBooks = books.slice(currentIndex, currentIndex + 5);
 
@@ -60,12 +53,9 @@ const RelatedBooks = () => {
             <div className="flex items-center justify-center flex-col md:flex-row mt-14">
                 <div className="flex space-x-4 mx-4">
                     {visibleBooks.map((book) => (
-                        <div
-                            key={book.id}
-                            className="w-[204px] h-[298px] bg-red-500 flex items-center justify-center text-white rounded"
-                        >
-                            {book.title}
-                        </div>
+                        <Link key={book.id} to={`/book/${book.id}`}>
+                            <img className="object-cover md:w-[204px] md:h-[298px] rounded-md" src={book.volumeInfo.imageLinks.thumbnail} alt="Book" />
+                        </Link>
                     ))}
                 </div>
                 <div className="flex mt-4 md:hidden">
