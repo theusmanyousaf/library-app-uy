d15b64d381ad9a76b6b670f6f698bdf14d8d5e87
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-06-28 12:21:00 +0500

    api

--- src/services/googleBooksService.ts
+++ src/services/googleBooksService.ts
@@ -1,17 +1,13 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const API_URL = 'https://www.googleapis.com/books/v1/volumes';
-const API_KEY = process.env.API_KEY;
 
 export const fetchBooks = async (query: string) => {
     try {
         const response = await axios.get(API_URL, {
             params: {
                 q: query,
-                key: API_KEY,
+                key: process.env.REACT_APP_API_KEY,
                 maxResults: 40,
             },
         });
