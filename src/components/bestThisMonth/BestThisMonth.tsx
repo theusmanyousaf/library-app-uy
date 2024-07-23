643e0214c342083dc65ad450b4ccde656f6f2bb5
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-07-23 18:26:44 +0500

    minor changes

--- src/components/bestThisMonth/BestThisMonth.tsx
+++ src/components/bestThisMonth/BestThisMonth.tsx
@@ -5,7 +5,6 @@ import { useScroll } from '../../hooks/useScroll';
 
 export default function BestThisMonth() {
     const { visibleBooks, status, error, value, handleMoreClick } = useScroll(14, 14, 10)
-    const { visibleBooks, status, error, value, handleMoreClick } = useScroll(14, 14, 10)
     if (status === 'loading') {
         return <div>Loading...</div>;
     }
